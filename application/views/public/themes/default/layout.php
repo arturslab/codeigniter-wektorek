dbf560d51879d43dba2158e2cffe21164746d270
Artur Słaboszewski <a.slaboszewski@gmail.com>
2020-06-27 03:52:30 +0200

    drobne poprawki

--- application/views/public/themes/default/layout.php
+++ application/views/public/themes/default/layout.php
@@ -1,6 +1,6 @@
 <?php if (isset($env)) {
     show_filename($env, __FILE__);
 } ?>
-<?php $this->load->view($this->config->item('ci_my_admin_template_dir_public') . 'header'); ?>
-<?php $this->load->view($this->config->item('ci_my_admin_template_dir_public') . 'content'); ?>
-<?php $this->load->view($this->config->item('ci_my_admin_template_dir_public') . 'footer'); ?>
+<?php $this->load->view($this->config->item('ci_my_admin_template_dir_public') . 'includes/header'); ?>
+<?php $this->load->view($this->config->item('ci_my_admin_template_dir_public') . 'includes/content'); ?>
+<?php $this->load->view($this->config->item('ci_my_admin_template_dir_public') . 'includes/footer'); ?>
